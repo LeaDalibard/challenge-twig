0a6026e75c9ff73629a5f448121150e9a8935789
Lea Dalibard <leadalibard@gmail.com>
2020-11-10 11:01:37 +0100

    Adding dropdown

--- src/Logger.php
+++ src/Logger.php
@@ -8,6 +8,7 @@ class Logger
 {
     public function log($message)
     {
-        file_put_contents(__DIR__.'log.info', $message, FILE_APPEND );
+        file_put_contents('log.info', $message, FILE_APPEND );
+        //file in in public directory
     }
 }
